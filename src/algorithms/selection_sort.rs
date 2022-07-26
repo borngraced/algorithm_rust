e44a76cde75b0b9e747c67ca65321bd99ac7838b
borngraced <samiodev@icloud.com>
2022-07-26 11:48:02 -0400

    selection_sort

---
+++ src/algorithms/selection_sort.rs
@@ -0,0 +1,23 @@
+/// Selection Sort
+///
+///
+/// Ascending Order: Sorts an array by repeatedly finding the smallest ement in a vector of
+/// elements
+///
+/// Descending Order: Sorts an array by repeatedly finding the biggest element in a vector of
+/// elements
+///
+/// Time Complexity - O(n^2).
+/// Reason - there are two nested object to iterate through
+///
+///
+///
+pub fn selection_sort() {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn selection_sort_test() {
+        let items = vec![9, 4, 6, 1, 7, 3, 2, 8, 5, 0];
+    }
+}
