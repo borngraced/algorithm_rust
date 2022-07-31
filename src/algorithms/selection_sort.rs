ca89d974ab06afadab624b64a39dc90439aa97d7
borngraced <samiodev@icloud.com>
2022-07-31 14:04:28 -0400

    selection_sort done

--- src/algorithms/selection_sort.rs
+++ src/algorithms/selection_sort.rs
@@ -21,14 +21,14 @@ pub fn selection_sort(mut arr: Vec<u32>) -> Vec<u32> {
         return vec![];
     }
 
-    for _ in 0..arr.len() {
-        if arr.len() >= 1 {
+    for _ in 0..len {
+        if len >= 1 {
             // find the index of the smallest element in the array
-            let smallest = get_smallest(&arr);
+            let smallest_idx = get_smallest_idx(&arr);
             // insert the smallest element into our new sorted_array variable
-            sorted_array.push(arr[smallest]);
+            sorted_array.push(arr[smallest_idx]);
             // finally remove the smallest array from our original array using the it's index
-            arr.remove(smallest);
+            arr.remove(smallest_idx);
         }
     }
 
@@ -36,7 +36,7 @@ pub fn selection_sort(mut arr: Vec<u32>) -> Vec<u32> {
 }
 
 /// get the smallest element in the array
-pub fn get_smallest(arr: &Vec<u32>) -> usize {
+pub fn get_smallest_idx(arr: &Vec<u32>) -> usize {
     let mut smallest_idx = 0;
     let mut smallest = arr[0];
 
@@ -52,13 +52,13 @@ pub fn get_smallest(arr: &Vec<u32>) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::algorithms::selection_sort::{get_smallest, selection_sort};
+    use crate::algorithms::selection_sort::{get_smallest_idx, selection_sort};
 
     #[test]
     fn selection_sort_test() {
         let items = vec![9, 4, 6, 1, 5, 7, 3, 2, 8, 10];
         let empty: Vec<u32> = vec![];
-        assert_eq!(3, get_smallest(&items));
+        assert_eq!(3, get_smallest_idx(&items));
         assert_eq!(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], selection_sort(items));
         assert_eq!(empty, selection_sort(vec![]));
     }
